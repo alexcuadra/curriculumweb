9bea6e1e160cf026d8f1672fa9801a8830a8c8dc
root <root@alex.alex>
2017-11-03 22:14:02 -0600

    agregando progerss bar para los skills

--- app/js/progress.js
+++ app/js/progress.js
@@ -1,45 +1,45 @@
 // graph1
-var el = document.getElementById('graph1'); // get canvas
+    var el = document.getElementById('graph1'); // get canvas
 
-var options = {
-    percent:  el.getAttribute('data-percent') || 25,
-    size: el.getAttribute('data-size') || 220,
-    lineWidth: el.getAttribute('data-line') || 15,
-    rotate: el.getAttribute('data-rotate') || 0
-}
+    var options = {
+        percent:  el.getAttribute('data-percent') || 25,
+        size: el.getAttribute('data-size') || 220,
+        lineWidth: el.getAttribute('data-line') || 25,
+        rotate: el.getAttribute('data-rotate') || 0
+    }
 
-var canvas = document.createElement('canvas');
-var span = document.createElement('span');
-span.textContent = options.percent + '%';
+    var canvas = document.createElement('canvas');
+    var span = document.createElement('span');
+    span.textContent = options.percent + '%';
 
-if (typeof(G_vmlCanvasManager) !== 'undefined') {
-    G_vmlCanvasManager.initElement(canvas);
-}
+    if (typeof(G_vmlCanvasManager) !== 'undefined') {
+        G_vmlCanvasManager.initElement(canvas);
+    }
 
-var ctx = canvas.getContext('2d');
-canvas.width = canvas.height = options.size;
+    var ctx = canvas.getContext('2d');
+    canvas.width = canvas.height = options.size;
 
-el.appendChild(span);
-el.appendChild(canvas);
+    el.appendChild(span);
+    el.appendChild(canvas);
 
-ctx.translate(options.size / 2, options.size / 2); // change center
-ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+    ctx.translate(options.size / 2, options.size / 2); // change center
+    ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
 
-//imd = ctx.getImageData(0, 0, 240, 240);
-var radius = (options.size - options.lineWidth) / 2;
+    //imd = ctx.getImageData(0, 0, 240, 240);
+    var radius = (options.size - options.lineWidth) / 2;
 
-var drawCircle = function(color, lineWidth, percent) {
-        percent = Math.min(Math.max(0, percent || 1), 1);
-        ctx.beginPath();
-        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
-        ctx.strokeStyle = color;
-        ctx.lineCap = 'round'; // butt, round or square
-        ctx.lineWidth = lineWidth
-        ctx.stroke();
-};
+    var drawCircle = function(color, lineWidth, percent) {
+            percent = Math.min(Math.max(0, percent || 1), 1);
+            ctx.beginPath();
+            ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+            ctx.strokeStyle = color;
+            ctx.lineCap = 'round'; // butt, round or square
+            ctx.lineWidth = lineWidth
+            ctx.stroke();
+    };
 
-drawCircle('#efefef', options.lineWidth, 100 / 100);
-drawCircle('#555555', options.lineWidth, options.percent / 100);
+    drawCircle('#efefef', options.lineWidth, 100 / 100);
+    drawCircle('#2980b9', options.lineWidth, options.percent / 100);
 
 // graph2
 
@@ -48,7 +48,7 @@ var el = document.getElementById('graph2'); // get canvas
 var options = {
     percent:  el.getAttribute('data-percent') || 25,
     size: el.getAttribute('data-size') || 220,
-    lineWidth: el.getAttribute('data-line') || 15,
+    lineWidth: el.getAttribute('data-line') || 25,
     rotate: el.getAttribute('data-rotate') || 0
 }
 
@@ -83,7 +83,7 @@ var drawCircle = function(color, lineWidth, percent) {
 };
 
 drawCircle('#efefef', options.lineWidth, 100 / 100);
-drawCircle('#555555', options.lineWidth, options.percent / 100);
+drawCircle('#2980b9', options.lineWidth, options.percent / 100);
 
 // graph3
 var el = document.getElementById('graph3'); // get canvas
@@ -91,7 +91,7 @@ var el = document.getElementById('graph3'); // get canvas
 var options = {
     percent:  el.getAttribute('data-percent') || 25,
     size: el.getAttribute('data-size') || 220,
-    lineWidth: el.getAttribute('data-line') || 15,
+    lineWidth: el.getAttribute('data-line') || 25,
     rotate: el.getAttribute('data-rotate') || 0
 }
 
@@ -126,4 +126,224 @@ var drawCircle = function(color, lineWidth, percent) {
 };
 
 drawCircle('#efefef', options.lineWidth, 100 / 100);
-drawCircle('#555555', options.lineWidth, options.percent / 100);
+drawCircle('#2980b9', options.lineWidth, options.percent / 100);
+
+
+
+
+
+// graph4
+    var el = document.getElementById('graph4'); // get canvas
+
+    var options = {
+        percent:  el.getAttribute('data-percent') || 25,
+        size: el.getAttribute('data-size') || 220,
+        lineWidth: el.getAttribute('data-line') || 25,
+        rotate: el.getAttribute('data-rotate') || 0
+    }
+
+    var canvas = document.createElement('canvas');
+    var span = document.createElement('span');
+    span.textContent = options.percent + '%';
+
+    if (typeof(G_vmlCanvasManager) !== 'undefined') {
+        G_vmlCanvasManager.initElement(canvas);
+    }
+
+    var ctx = canvas.getContext('2d');
+    canvas.width = canvas.height = options.size;
+
+    el.appendChild(span);
+    el.appendChild(canvas);
+
+    ctx.translate(options.size / 2, options.size / 2); // change center
+    ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+    //imd = ctx.getImageData(0, 0, 240, 240);
+    var radius = (options.size - options.lineWidth) / 2;
+
+    var drawCircle = function(color, lineWidth, percent) {
+            percent = Math.min(Math.max(0, percent || 1), 1);
+            ctx.beginPath();
+            ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+            ctx.strokeStyle = color;
+            ctx.lineCap = 'round'; // butt, round or square
+            ctx.lineWidth = lineWidth
+            ctx.stroke();
+    };
+
+    drawCircle('#efefef', options.lineWidth, 100 / 100);
+    drawCircle('#2980b9', options.lineWidth, options.percent / 100);
+
+// graph5
+
+var el = document.getElementById('graph5'); // get canvas
+
+var options = {
+    percent:  el.getAttribute('data-percent') || 25,
+    size: el.getAttribute('data-size') || 220,
+    lineWidth: el.getAttribute('data-line') || 25,
+    rotate: el.getAttribute('data-rotate') || 0
+}
+
+var canvas = document.createElement('canvas');
+var span = document.createElement('span');
+span.textContent = options.percent + '%';
+
+if (typeof(G_vmlCanvasManager) !== 'undefined') {
+    G_vmlCanvasManager.initElement(canvas);
+}
+
+var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = options.size;
+
+el.appendChild(span);
+el.appendChild(canvas);
+
+ctx.translate(options.size / 2, options.size / 2); // change center
+ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+//imd = ctx.getImageData(0, 0, 240, 240);
+var radius = (options.size - options.lineWidth) / 2;
+
+var drawCircle = function(color, lineWidth, percent) {
+        percent = Math.min(Math.max(0, percent || 1), 1);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineCap = 'round'; // butt, round or square
+        ctx.lineWidth = lineWidth
+        ctx.stroke();
+};
+
+drawCircle('#efefef', options.lineWidth, 100 / 100);
+drawCircle('#2980b9', options.lineWidth, options.percent / 100);
+
+// graph6
+var el = document.getElementById('graph6'); // get canvas
+
+var options = {
+    percent:  el.getAttribute('data-percent') || 25,
+    size: el.getAttribute('data-size') || 220,
+    lineWidth: el.getAttribute('data-line') || 25,
+    rotate: el.getAttribute('data-rotate') || 0
+}
+
+var canvas = document.createElement('canvas');
+var span = document.createElement('span');
+span.textContent = options.percent + '%';
+
+if (typeof(G_vmlCanvasManager) !== 'undefined') {
+    G_vmlCanvasManager.initElement(canvas);
+}
+
+var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = options.size;
+
+el.appendChild(span);
+el.appendChild(canvas);
+
+ctx.translate(options.size / 2, options.size / 2); // change center
+ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+//imd = ctx.getImageData(0, 0, 240, 240);
+var radius = (options.size - options.lineWidth) / 2;
+
+var drawCircle = function(color, lineWidth, percent) {
+        percent = Math.min(Math.max(0, percent || 1), 1);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineCap = 'round'; // butt, round or square
+        ctx.lineWidth = lineWidth
+        ctx.stroke();
+};
+
+drawCircle('#efefef', options.lineWidth, 100 / 100);
+drawCircle('#2980b9', options.lineWidth, options.percent / 100);
+
+// graph7
+var el = document.getElementById('graph7'); // get canvas
+
+var options = {
+    percent:  el.getAttribute('data-percent') || 25,
+    size: el.getAttribute('data-size') || 220,
+    lineWidth: el.getAttribute('data-line') || 25,
+    rotate: el.getAttribute('data-rotate') || 0
+}
+
+var canvas = document.createElement('canvas');
+var span = document.createElement('span');
+span.textContent = options.percent + '%';
+
+if (typeof(G_vmlCanvasManager) !== 'undefined') {
+    G_vmlCanvasManager.initElement(canvas);
+}
+
+var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = options.size;
+
+el.appendChild(span);
+el.appendChild(canvas);
+
+ctx.translate(options.size / 2, options.size / 2); // change center
+ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+//imd = ctx.getImageData(0, 0, 240, 240);
+var radius = (options.size - options.lineWidth) / 2;
+
+var drawCircle = function(color, lineWidth, percent) {
+        percent = Math.min(Math.max(0, percent || 1), 1);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineCap = 'round'; // butt, round or square
+        ctx.lineWidth = lineWidth
+        ctx.stroke();
+};
+
+drawCircle('#efefef', options.lineWidth, 100 / 100);
+drawCircle('#2980b9', options.lineWidth, options.percent / 100);
+
+// graph8
+var el = document.getElementById('graph8'); // get canvas
+
+var options = {
+    percent:  el.getAttribute('data-percent') || 25,
+    size: el.getAttribute('data-size') || 220,
+    lineWidth: el.getAttribute('data-line') || 25,
+    rotate: el.getAttribute('data-rotate') || 0
+}
+
+var canvas = document.createElement('canvas');
+var span = document.createElement('span');
+span.textContent = options.percent + '%';
+
+if (typeof(G_vmlCanvasManager) !== 'undefined') {
+    G_vmlCanvasManager.initElement(canvas);
+}
+
+var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = options.size;
+
+el.appendChild(span);
+el.appendChild(canvas);
+
+ctx.translate(options.size / 2, options.size / 2); // change center
+ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+//imd = ctx.getImageData(0, 0, 240, 240);
+var radius = (options.size - options.lineWidth) / 2;
+
+var drawCircle = function(color, lineWidth, percent) {
+        percent = Math.min(Math.max(0, percent || 1), 1);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineCap = 'round'; // butt, round or square
+        ctx.lineWidth = lineWidth
+        ctx.stroke();
+};
+
+drawCircle('#efefef', options.lineWidth, 100 / 100);
+drawCircle('#2980b9', options.lineWidth, options.percent / 100);
